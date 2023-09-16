dba05fc4b186665fbe32095127afec65d299d93c
Vishwanath Waghmare <102869329+vishwanathwaghmare@users.noreply.github.com>
2023-09-17 00:46:27 +0530

    new file uploaded

---
+++ OppsConcept/Methods/Account.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OppsConcept.Methods
+{
+    //create class Account containing the following methods => insert(),display(),deposit(),withdraw(),checkBalance().
+    public class Account
+    {
+        public int acc_no { get; set; }
+        public string Name { get;set; }
+        public string acc_type { get;set; }
+        public float amount { get;set; }
+
+        public Account(int acc_no, string Name, string acc_type, float amount)
+        {
+            this.acc_no = acc_no;
+            this.Name = Name;
+            this.acc_type = acc_type;
+            this.amount = amount;
+
+        }
+
+        public void insert(int acc_no, string Name, string acc_type,float amount)
+        {
+            this.acc_no = acc_no;
+            this.Name= Name;
+            this.acc_type = acc_type;
+            this.amount = amount;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Account Number:"+acc_no);
+            Console.WriteLine("Name of Acc Holder:"+Name);
+            Console.WriteLine("Account type:"+acc_type);
+            Console.WriteLine("Current Balance:"+amount);
+        }
+        public void Deposit(float depAmount)
+        {
+            if (depAmount > 0)
+            {
+                amount = amount+depAmount;
+                Console.WriteLine("Amount Deposited Successfully......");
+            }
+        }
+        public void Withdraw(float withAmount)
+        {
+            if(withAmount>0&& withAmount < amount)
+            {
+                amount = amount-withAmount;
+                Console.WriteLine("Amount Withdrawn successfully.....");
+            }
+            else
+            {
+                Console.WriteLine("Balance insufficient to withdraw");
+            }
+        }
+        public void CheckBalance()
+        {
+            Console.WriteLine("Account Balance is:"+amount);
+        }
+
+    }
+
+
+
+
+    class AccMethod
+    {
+        static void Main(string[] args)
+        {
+            Account account=new Account(101,"Vishwanath","Saving",10000.0f);
+
+            account.Display();
+            account.Deposit(5000);
+            account.CheckBalance();
+            account.Withdraw(3000);
+            account.CheckBalance();
+
+        }
+    }
+}
---
+++ OppsConcept/Methods/CubeCalcultor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcept.Methods
+{
+    public class CubeCalcultor
+    {
+        // no return type no parameter
+        public void CalculateCube()
+        {
+            Console.Write("Enter a number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            double cube = num * num * num;
+            Console.WriteLine($"The cube of {num} is {cube}");
+        }
+
+        //With Return Type, No Parameters:
+        public double CalculateCube1()
+        {
+            Console.Write("Enter a number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            double cube = num * num * num;
+            return cube;
+        }
+
+        // No Return Type, With Parameter Passing:
+        public void CalculateCube2(double num)
+        {
+            double cube = num * num * num;
+            Console.WriteLine($"The cube of {num} is {cube}");
+        }
+
+        //With Return Type, With Parameter Passing
+        public double CalculateCube3(double num)
+        {
+            double cube = num * num * num;
+            return cube;
+        }
+    }
+
+    public class CalcCube
+    {
+        static void Main(string[] args)
+        {
+            CubeCalcultor calculator = new CubeCalcultor();
+            calculator.CalculateCube();
+
+            CubeCalcultor calculator1 = new CubeCalcultor();
+            double result = calculator.CalculateCube1();
+            Console.WriteLine($"The cube of the number is {result}");
+
+            CubeCalcultor calculator2 = new CubeCalcultor();
+            Console.Write("Enter a number: ");
+            double input = Convert.ToDouble(Console.ReadLine());
+            calculator.CalculateCube2(input);
+
+            CubeCalcultor calculator3 = new CubeCalcultor();
+            Console.Write("Enter a number: ");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            double result1 = calculator.CalculateCube3(input);
+            Console.WriteLine($"The cube of the number is {result}");
+        }
+    }
+}
