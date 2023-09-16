1f5cb2aa43b316132c317252a552f508248c0c4b
Vishwanath Waghmare <102869329+vishwanathwaghmare@users.noreply.github.com>
2023-09-17 00:49:58 +0530

    polymorphism assignment

---
+++ OppsConcept/Inheritance/Kid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcept.Polymorphism
+{
+    /* class Kid
+    {
+        public void ReadBook()
+        {
+            Console.WriteLine("Kid is Reading Book.");
+        }
+        public void ReadBook(string bookTitle,string Author)
+        {
+            Console.WriteLine($"kid is reading {bookTitle} by {Author}");
+        }
+    }
+     class BigKid: Kid
+    {
+        public override void ReadBook()
+         
+        {
+            Console.WriteLine("Big Kid is reading bigger book");
+        }
+    }
+    class KidMain
+    {
+        static void Main(string[] args)
+        {
+            Kid kid = new Kid();
+            kid.ReadBook();
+            kid.ReadBook("harry potter","j.k.rowling");
+
+            BigKid kid2 = new BigKid();
+            kid2.ReadBook();
+            kid2.ReadBook("Lord Buddha", "DR.Ambedkar");
+        }
+    }*/
+}
---
+++ OppsConcept/Inheritance/Teenager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcept.Polymorphism
+{
+
+    using System;
+
+    class Kid
+    {
+        public virtual void readBook()
+        {
+            Console.WriteLine("Kid is reading a picture book.");
+        }
+    }
+
+    class BigKid : Kid
+    {
+        public override void readBook()
+        {
+            Console.WriteLine("BigKid is reading an adventure book.");
+        }
+    }
+
+    class Teenager : Kid
+    {
+        public override void readBook()
+        {
+            Console.WriteLine("Teenager is reading a novel.");
+        }
+    }
+
+    class Teenager1
+    {
+        static void Main(string[] args)
+        {
+            Kid k1 = new BigKid();
+            Kid k2 = new Teenager();
+
+            k1.readBook(); // Calls BigKid's implementation
+            k2.readBook(); // Calls Teenager's implementation
+        }
+    }
+
+}
