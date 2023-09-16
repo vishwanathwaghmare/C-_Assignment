feb73d7c6c949d460709293cd8d2b432dae28cff
Vishwanath Waghmare <102869329+vishwanathwaghmare@users.noreply.github.com>
2023-09-17 01:20:28 +0530

    new file uploaded

---
+++ OppsConcept/Exception_Handling/Alphabet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    using System;
+
+    // WAP to check String content only alphabets if any digit or symbol is present then throw custom exception
+    public class NonAlphabeticException : Exception
+    {
+        public NonAlphabeticException() : base("String contains non-alphabetic characters.")
+        {
+        }
+    }
+
+    class AlphabetOnly
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string inputString = "Hello123"; // Replace with your input string
+
+                if (!IsAlphabetic(inputString))
+                {
+                    throw new NonAlphabeticException();
+                }
+
+                Console.WriteLine($"The string is: {inputString}");
+            }
+            catch (NonAlphabeticException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                // Handle the exception or perform any necessary actions here
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        // Function to check if a string contains only alphabetic characters
+        static bool IsAlphabetic(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
---
+++ OppsConcept/Exception_Handling/NegativeNumberException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    using System;
+
+    // If number is negative then throw negative number exception.
+    public class NegativeNumberException : Exception
+    {
+        public NegativeNumberException() : base("Negative numbers are not allowed.")
+        {
+        }
+    }
+
+    class NegativeNum
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = -5; // Replace with your number
+
+                if (number < 0)
+                {
+                    throw new NegativeNumberException();
+                }
+
+                Console.WriteLine($"The number is: {number}");
+            }
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                // Handle the exception or perform any necessary actions here
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+
+}
