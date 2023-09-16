918eef2ce01c67db386534b750afa7d77f55ede3
Vishwanath Waghmare <102869329+vishwanathwaghmare@users.noreply.github.com>
2023-09-17 01:27:06 +0530

    new file uploaded

---
+++ OppsConcept/Exception_Handling/FinallyUse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    using System;
+
+    class FinallyUse
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                int result = Divide(10, 0);
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                // Code inside the finally block will always be executed
+                Console.WriteLine("Finally block executed.");
+            }
+
+            Console.WriteLine("After try-catch-finally block.");
+        }
+
+        static int Divide(int dividend, int divisor)
+        {
+            try
+            {
+                return dividend / divisor;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error in Divide method: {ex.Message}");
+                // Re-throwing the exception for higher-level handling
+                throw;
+            }
+        }
+    }
+
+}
