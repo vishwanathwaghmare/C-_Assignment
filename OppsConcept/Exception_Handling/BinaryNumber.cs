540fb46598b5f6f29a2e26da8319e31326a09166
Vishwanath Waghmare <102869329+vishwanathwaghmare@users.noreply.github.com>
2023-09-17 01:50:52 +0530

    new file added

---
+++ OppsConcept/Exception_Handling/BinaryNumber.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    using System;
+
+    // Custom exception class for invalid binary input
+    public class InvalidBinaryException : Exception
+    {
+        public InvalidBinaryException(string message) : base(message)
+        {
+        }
+    }
+
+    class BinaryNumber
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a binary number: ");
+                string binaryInput = Console.ReadLine();
+
+                // Check if the input is a valid binary number
+                if (IsBinary(binaryInput))
+                {
+                    int decimalValue = ConvertBinaryToDecimal(binaryInput);
+                    Console.WriteLine($"Decimal equivalent: {decimalValue}");
+                }
+                else
+                {
+                    throw new InvalidBinaryException("Invalid binary number.");
+                }
+            }
+            catch (InvalidBinaryException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        static bool IsBinary(string input)
+        {
+            foreach (char c in input)
+            {
+                if (c != '0' && c != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static int ConvertBinaryToDecimal(string binary)
+        {
+            int decimalValue = 0;
+            int power = 0;
+
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                if (binary[i] == '1')
+                {
+                    decimalValue += (int)Math.Pow(2, power);
+                }
+                power++;
+            }
+
+            return decimalValue;
+        }
+    }
+
+}
---
+++ OppsConcept/Exception_Handling/MultiCatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    using System;
+
+    class MultiCatch
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            try
+            {
+                // Code that may throw exceptions
+                int[] numbers = null;
+                int result = n/ 0;
+                string str = "Hello";
+                char ch = str[10];
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Caught DivideByZeroException: {ex.Message}");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Caught IndexOutOfRangeException: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Caught Exception: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed.");
+            }
+
+            Console.WriteLine("After try-catch-finally block.");
+        }
+    }
+
+}
