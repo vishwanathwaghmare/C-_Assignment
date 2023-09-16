1f7fffdfac1bda904206f5f8f2408d0f2852e66c
Vishwanath Waghmare <102869329+vishwanathwaghmare@users.noreply.github.com>
2023-09-17 01:44:32 +0530

    new file uploaded

---
+++ OppsConcept/Exception_Handling/Propagation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    using System;
+
+    class Propagation
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Main method: Start");
+                DivideByZeroMethod();
+                Console.WriteLine("Main method: End");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Main method: Caught an exception: {ex.Message}");
+            }
+        }
+
+        static void DivideByZeroMethod()
+        {
+            try
+            {
+                Console.WriteLine("DivideByZeroMethod: Start");
+                int result = Divide(10, 0); // This will throw an exception
+                Console.WriteLine($"Result: {result}");
+                Console.WriteLine("DivideByZeroMethod: End");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"DivideByZeroMethod: Caught an exception: {ex.Message}");
+                // Re-throw the exception to propagate it up the call stack
+                throw;
+            }
+        }
+
+        static int Divide(int dividend, int divisor)
+        {
+            Console.WriteLine("Divide: Start");
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            int result = dividend / divisor;
+            Console.WriteLine("Divide: End");
+            return result;
+        }
+    }
+
+}
---
+++ OppsConcept/Exception_Handling/UnHandelException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    using System;
+
+    class UnHandelException
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Main method: Start");
+                UnhandledExceptionMethod();
+                Console.WriteLine("Main method: End");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Main method: Caught an exception: {ex.Message}");
+            }
+        }
+
+        static void UnhandledExceptionMethod()
+        {
+            Console.WriteLine("UnhandledExceptionMethod: Start");
+
+            // This line will throw an unhandled exception
+            int result = 10;
+
+            Console.WriteLine("UnhandledExceptionMethod: End");
+        }
+    }
+
+}
